e68c0c8fb8aa8b0313787164960b92b9099f59f4
Kaio Rodrigues <76459361+Kaiorc@users.noreply.github.com>
2022-07-13 02:59:32 -0300

    Update and rename main.cpp to Pilha.cpp

--- Pilha.cpp
+++ Pilha.cpp
@@ -1,328 +1,184 @@
-//Kaio Rodrigues da Cunha
-//1394485
+//Kaio Rodrigues
 
 #include <iostream>
 
-
-
 using namespace std;
-
-
 
 int const MAXTAM = 10;
 
-
-
 class Pilha{
-
 	private :
-
 		int vetor[MAXTAM]; //pilha
-
-        int topo; // topo da pilha
-
-
+        	int topo; // topo da pilha
 
    	public:
-
    		//Construtor da Classe
-
 		Pilha(){
-
 			this->topo= -1;
-
 		}
-
-
 
 		//Apaga a Pilha
-
 		void Clear(){
-
 		     this ->topo = -1;
-
 		}
-
-
 
 		//Verifica se a pilha esta vazia
-
 		bool isEmpty(){
-
 			if(topo == -1){
-
 				return true;
-
 			} else{
-
 				return false;
-
 			}
-
 		}
-
-
 
 		//Verifica se a Pilha esta cheia
-
 		bool isFull(){
-
 			if(topo == MAXTAM-1){
-
 				return true;
-
 			} else{
-
 				return false;
-
 			}
-
 		}
 
-
-
 		//Insere um elemento na Pilha
-
 		void Push( int info){
-
 			if(isFull()){
-
 				cout<<"A Pilha ja esta cheia "<<endl;
-
 			}else{
-
 			   vetor[topo+1] = info;
-
 			    topo ++;
-
 			}
-
         }
 
-
-
         //Remove elemento da Pilha e retorna o elemento que foi removido
-
         int Pop(){
-
          int guarda;
-
-		 if(isEmpty()){
-
-           cout<<"A Pilha se encontra vazia"<<endl<<endl;
-
-			} else{
-
+		if(isEmpty()){
+           		cout<<"A Pilha se encontra vazia"<<endl<<endl;
+		} else{
 			  guarda = vetor[topo];
-
 			  vetor[topo] == NULL;
-
 			  topo --;
-
 			}
-
 			return guarda;
-
 		}
 
 		//Busca na Pilha
-
 		int Search(int elemento){
-
-		   int status = 0; //Guardar· o elemento se o elemento est· ou nao na Pilha
-
+		   int status = 0; //Guardar√° o elemento se o elemento est√° ou nao na Pilha
 		   int guarda;
-
 		   Pilha aux;
 
 		   //Caso a pilha esteja vazia
-
 		   if(topo == -1){
-
 			   status = 0;
-
 		   }
 
-		   //Caso a Pilha sÛ tenha um elemento
-
+		   //Caso a Pilha s√≥ tenha um elemento
 		   if(topo == 0){
-
 			   guarda = Pop();
-
 			   if(guarda == elemento){
-
 				   status  = 1;
-
 				   Push(guarda);
-
 			   } else{
-
 				   status = 0;
-
 				   Push(guarda);
-
 			   }
-
 		   }
 
 		   //Caso a Pilha tenha mais de um elemento
-
 		   if(topo > 0){
+			int cont = topo;
+			guarda = Pop();
 
-           int cont = topo;
-
-		   guarda = Pop();
-
-		   //Compara se o elemento que se est· buscando È o primeiro da Pilha
-
+		   //Compara se o elemento que se est√° buscando √© o primeiro da Pilha
 		    if(guarda == elemento){
-
-			  status =1;
-
-			  Push(elemento);
-
-           // Se o elemento n„o estiver na 1∫ PosiÁ„o*/
-
-		   } else{
-
+			status =1;
+			Push(elemento);
+			// Se o elemento n√£o estiver na 1¬∫ Posi√ß√£o*/
+			} else{
 		   	 aux.Push(guarda);
-
-		   	 // Desempilha atÈ achar o elemento e caso n„o esteja status continua com 0
-
+		   	 // Desempilha at√© achar o elemento e caso n√£o esteja status continua com 0
 			  while(topo > -1){
-
 				  guarda = Pop();
-
 				  aux.Push(guarda);
-
 				  cont --;
-
 				  if(guarda == elemento){
-
 					  status = 1;
-
 					  break;
-
 				  }
-
 				  if(cont == 0){
-
 					  break;
-
 				  }
-
 			  }
-
-			  //Processo que ir· empilhar na Pilha novamente
-
+			    
+			  //Processo que ir√° empilhar na Pilha novamente
 			  while(aux.topo!=-1){
-
 				  Push(aux.Pop());
-
 			  }
-
 		    }
-
 		   }
-
 		   return status;
-
 		}
 };
 
 
-
 int main(int argc, char** argv){
-
+	
    Pilha p1;
-
    int item;
 
    do{
-	   cout<<"1 - Inserir"<<endl;
+	cout<<"1 - Inserir"<<endl;
+	cout<<"2 - Remover"<<endl;
+	cout<<"3 - Buscar Elemento "<<endl;
+	cout<<"4 - Apagar Pilha"<<endl;
+	cout<<"5 - Sair"<<endl<<endl;
+	cout<<"Insira o numero da operacao a ser realizada: ";
 
-	   cout<<"2 - Remover"<<endl;
+	cin>>item;
 
-	   cout<<"3 - Buscar Elemento "<<endl;
-
-	   cout<<"4 - Apagar Pilha"<<endl;
-
-	   cout<<"5 - Sair"<<endl<<endl;
-
-	   cout<<"Insira o numero da operacao a ser realizada: ";
-
-	   cin>>item;
-
-	   cout<<endl<<endl;
+	cout<<endl<<endl;
 
 
         if(item == 1){
-
-		   cout<<"Insira o elemento que deseja colocar na Pilha: ";
-
-		   int elemento;
-
-		   cin>>elemento;
-
-		   p1.Push(elemento);
-
-		   cout<<endl<<endl;
-
+	   cout<<"Insira o elemento que deseja colocar na Pilha: ";
+	   int elemento;
+	   cin>>elemento;
+	   p1.Push(elemento);
+	   cout<<endl<<endl;
 	   }
 
 	    else if(item == 2){
-
   	      if(!p1.isEmpty()){
-
 		    cout<<"Elemento: "<<p1.Pop()<<" "<<"foi removido da Pilha"<<endl<<endl;
-
           } else {
-
-			  p1.Pop();
-
+		      p1.Pop();
 		  }
-
 	   }
-
 
 	   else if(item == 3){
+		cout<<"Insira o elemento que deseja buscar na Pilha: ";
 
-		   cout<<"Insira o elemento que deseja buscar na Pilha: ";
+		int elemento;
 
-		   int elemento;
+		cin>>elemento;
 
-		   cin>>elemento;
+		cout<<endl<<endl;
 
-		   cout<<endl<<endl;
-
-		   if(p1.Search(elemento) == 1){
-
-			   cout<<"O elemento se encontra na Pilha"<<endl<<endl;
-
-		   } else{
-
-			   cout<<"O elemento nao esta na Pilha"<<endl<<endl;
-
-		   }
-
+		if(p1.Search(elemento) == 1){
+			cout<<"O elemento se encontra na Pilha"<<endl<<endl;
+		} else{
+			cout<<"O elemento nao esta na Pilha"<<endl<<endl;
+		}
 	   }
-
-
 
 	   else if(item == 4){
-
 		   p1.Clear();
-
 	   }
-
 	       else if(item < 1 || item > 5){
-
 		   cout<<"Opcao Invalida"<<endl<<endl;
-
 	   }
-
-
 
    } while(item != 5);
-
 }
